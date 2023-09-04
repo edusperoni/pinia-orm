604d07248626eb77bd05f5946edafead1e88ebed
Eduardo Speroni <edusperoni@gmail.com>
2023-09-04 20:55:17 -0300

    feat: improve performance by mutating the state

--- packages/pinia-orm/src/composables/useStoreActions.ts
+++ packages/pinia-orm/src/composables/useStoreActions.ts
@@ -5,19 +5,19 @@ import type { DataStore } from './useDataStore'
 export function useStoreActions(query?: Query) {
   return {
     save(this: DataStore, records: Elements, triggerQueryAction = true) {
-      this.data = { ...this.data, ...records }
+      Object.assign(this.data, records)
 
       if (triggerQueryAction && query)
         query.newQuery(this.$id).save(Object.values(records))
     },
     insert(this: DataStore, records: Elements, triggerQueryAction = true) {
-      this.data = { ...this.data, ...records }
+      Object.assign(this.data, records)
 
       if (triggerQueryAction && query)
         query.newQuery(this.$id).insert(Object.values(records))
     },
     update(this: DataStore, records: Elements, triggerQueryAction = true) {
-      this.data = { ...this.data, ...records }
+      Object.assign(this.data, records)
 
       if (triggerQueryAction && query)
         query.newQuery(this.$id).update(Object.values(records))
@@ -29,37 +29,19 @@ export function useStoreActions(query?: Query) {
         query.newQuery(this.$id).fresh(Object.values(records))
     },
     destroy(this: DataStore, ids: (string | number)[], triggerQueryAction = true): void {
-      if (triggerQueryAction && query) {
+      if (triggerQueryAction && query)
         query.newQuery(this.$id).newQuery(this.$id).destroy(ids)
-      }
-      else {
-        const data: Elements = {}
-
-        for (const id in this.data) {
-          if (!ids.includes(id))
-            data[id] = this.data[id]
-        }
-
-        this.data = data
-      }
+      else
+        ids.forEach(id => delete this.data[id])
     },
     /**
      * Commit `delete` change to the store.
      */
     delete(this: DataStore, ids: (string | number)[], triggerQueryAction = true): void {
-      if (triggerQueryAction && query) {
+      if (triggerQueryAction && query)
         query.whereId(ids).delete()
-      }
-      else {
-        const data: Elements = {}
-
-        for (const id in this.data) {
-          if (!ids.includes(id))
-            data[id] = this.data[id]
-        }
-
-        this.data = data
-      }
+      else
+        ids.forEach(id => delete this.data[id])
     },
     flush(this: DataStore, _records?: Elements, triggerQueryAction = true): void {
       this.data = {}
